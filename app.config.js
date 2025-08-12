d7d69bdc6702b62093e1884634e8e54b7b530d02
Tishek <frantisekkresnak@gmail.com>
2025-08-12 19:00:03 +0200

    TestFlight

--- app.config.js
+++ app.config.js
@@ -1,11 +1,40 @@
-// app.config.js
 export default ({ config }) => ({
   ...config,
   expo: {
     ...(config.expo || {}),
-    runtimeVersion: { policy: 'sdkVersion' }, // nutné pro OTA v Expo Go
+
+    name: 'iWash',          // ← text pod ikonou v iOS
+    slug: 'iwashapp',       // může zůstat takhle
+
+    owner: 'tishek',
+    runtimeVersion: { policy: 'sdkVersion' },
+    updates: {
+      ...(config.expo?.updates || {}),
+      url: 'https://u.expo.dev/921ccb43-3892-4acd-9400-1d20af542edb',
+      enabled: true,
+      checkAutomatically: 'ON_LOAD',
+    },
+
+    icon: './assets/icon.png',
+
+    ios: {
+      ...(config.expo?.ios || {}),
+      bundleIdentifier: 'com.tishek.iwash',
+      infoPlist: {
+        ...(config.expo?.ios?.infoPlist || {}),
+        CFBundleDisplayName: 'iWash', // ← vynucení názvu pro iOS
+        NSLocationWhenInUseUsageDescription:
+          'Aplikace iWash používá vaši polohu k vyhledání nejbližších myček aut.',
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+
     extra: {
       ...(config.expo?.extra || {}),
+      eas: {
+        ...(config.expo?.extra?.eas || {}),
+        projectId: '921ccb43-3892-4acd-9400-1d20af542edb',
+      },
       googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
     },
   },
